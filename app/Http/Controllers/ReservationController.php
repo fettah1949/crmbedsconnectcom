c2646ae29a4f0e1a4954abd400ff3428b15c2730
fettah1949 <fattahallam1949@gmail.com>
2025-03-07 22:05:19 +0000

    SM-master fix res 3

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -32,7 +32,7 @@ class ReservationController extends Controller
       
             //    die('ffffff');
         //    ReservationController::crono_taux();
-         ReservationController::getdata();
+       return  ReservationController::getdata();
           ReservationController::commission_rese();
    
 
